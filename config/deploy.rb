7843367c03c79ad03d8a9086acd5cb73a2d2c9b4
Peter Glennon <pcrglennon@gmail.com>
2016-02-20 17:05:13 -0500

    chore(Capistrano): Set up Capistrano & production docker-compose config
    App runs on port 9292 (for now)
    No authentication on Redis server (yet)

---
+++ config/deploy.rb
@@ -0,0 +1,10 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'url_shortener'
+set :repo_url, 'git@github.com:pcrglennon/url_shortener.git'
+set :branch, 'capistrano-setup'
+
+set :linked_files, %w{config/redis.yml}
+
+set :deploy_to, '/var/www/url_shortener'
---
+++ config/deploy/production.rb
@@ -0,0 +1,29 @@
+role :docker, %w{docker-user@104.236.201.96}
+
+server '104.236.201.96', user: 'docker-user', roles: %w{docker}
+
+namespace :docker do
+  desc 'Replace linked files with copies of their targets, so Docker can add them properly'
+  task :copy_shared_files do
+    on roles(:docker) do |host|
+      within current_path do
+        fetch(:linked_files, []).each do |file|
+          execute 'cp', '--remove-destination', "$(readlink #{file})", file
+        end
+      end
+    end
+  end
+
+  task :setup_container => :copy_shared_files do
+    on roles(:docker) do |host|
+      within current_path do
+        execute 'docker-compose', 'stop', 'web'
+        execute 'docker-compose', 'rm', '-fv', 'web'
+        execute 'docker-compose', 'build'
+        execute 'docker-compose', '-f', 'docker-compose.production.yml', 'up', '-d'
+      end
+    end
+  end
+end
+
+after 'deploy:finishing', 'docker:setup_container'

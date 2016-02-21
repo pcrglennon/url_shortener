e713478165d7c95b20b43dbb57c44112268c4dca
Peter Glennon <pcrglennon@gmail.com>
2016-02-21 18:07:15 -0500

    fix(Capistrano): Pull from master branch (oops)

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.4.0'
 
 set :application, 'url_shortener'
 set :repo_url, 'git@github.com:pcrglennon/url_shortener.git'
-set :branch, 'capistrano-setup'
+set :branch, 'master'
 
 set :linked_files, %w{config/redis.yml}
 
